5c303e5bebec763ff18061ec8d3ee441ea3e3972
krEmch0 <emchovasilev127@outlook.com>
2025-05-12 22:03:55 +0300

    updated

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,7 +115,7 @@ LOC_HIDL_VERSION := 3.0
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(VENDOR_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
